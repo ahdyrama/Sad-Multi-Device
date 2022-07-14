03fa1e0118c1b1ee97a3f5b43cff98048965d9ee
Ramabotz9 <108044415+Ramabotz9@users.noreply.github.com>
2022-07-14 12:14:19 +0800

    Update info-bioowner.js

--- plugins/info-bioowner.js
+++ plugins/info-bioowner.js
@@ -2,23 +2,23 @@ let handler = async (m, { conn }) => {
 let ppown = await conn.profilePictureUrl(nomorown + '@s.whatsapp.net', 'image').catch(_ => hwaifu[1]) 
 let teksbio = `*───────[ BIODATA OWNER ]───────*
 *💌 Nama* : ᴅᴀɴɪ-ʙᴏᴛᴢ
-*✉️ Nama RL* : ᴅᴀɴɪ ᴏғғᴄɪᴀʟ
+*✉️ Nama RL* : Ahmad Bisa di Panggil Rama
 *♂️ Gender* : Laki - laki
 *🕋 Agama* : Islam
-*⏰ Tanggal lahir* : 16 Desember 2007
-*🎨 Umur* : 15
-*🧮 Kelas* : 9
+*⏰ Tanggal lahir* : 3 SEPTEMBER 1999
+*🎨 Umur* : 19
+*🧮 Kelas* : KULIAH
 *🧩 Hobby* : Nonton Hanime, Chatting, Recode script bot
 *💬 Sifat* : Idiot, Tidak Ramah, Kanjut, Prik, Pedofil
-*🗺️ Tinggal* : Indo, Kalsel, Kandangan, Hss
+*🗺️ Tinggal* : Indo, NTB. Mataram
 *❤️ Suka* : warnah kuning & biru, Hanime, waifu, loli, trap, furry, kucing
 *💔 Benci* : autis, anak epep, seleb
 
 *───────[ SOSIAL MEDIA ]───────*
-*📷 instagran* : @Dani_Offcial
-*🇫  Facebook* : Dani_Offc
-*🏮 Chanel Youtube* : DaniBotz
-*🐈 Github:* YTDANIBOTZ
+*📷 instagran* : @ahmdzakaria47
+*🇫  Facebook* : PSIKOPET
+*🏮 Chanel Youtube* : https://youtube.com/channel/UCJNi-zXIsky4nheONnxoY1w
+*🐈 Github:* RamaBotz9
 
 `
 conn.sendHydrated(m.chat, teksbio, wm, ppown, "wa.me/" + nomorown, "💬 ᴄʜᴀᴛs", null,null, [["ᴅᴏɴᴀsɪ", '.donasi'], [null, null],[null,null]], m)
