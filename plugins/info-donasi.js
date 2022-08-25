48c85e922b5dce1695b2416757769ce3bd40b030
Ramabotz9 <108044415+Ramabotz9@users.noreply.github.com>
2022-08-25 16:01:01 +0800

    Update info-donasi.js

--- plugins/info-donasi.js
+++ plugins/info-donasi.js
@@ -4,8 +4,8 @@ let pp = await conn.profilePictureUrl(m.chat).catch(_ => null)
 let pepe = pp ? await (await fetch(pp)).buffer() : Buffer.alloc(0)
 let str = ` ${conn.getName(m.sender)} Want Support Bot?
 *PAYMENT ↓*
-_*Pulsa/Pulse(XL):*_ 087793236775
-_*Dana/Gopay:*_ KOSONG
+_*Pulsa/Pulse(XL):6287793236775*_ 
+_*Dana/Gopay:085338890541*_ 
 Setelah melakukan donasi kirim bukti pembayaran ke owner,Dengan melakukan donasi,kamu telah mensupport bot ini agar on 24 jam dan fast respon
 `
 conn.sendHydrated(m.chat, str, wm, pepe, 'https://github.com/YTDANIBOTZ/', '𝙶𝙸𝚃𝙷𝚄𝙱', null, null, [
